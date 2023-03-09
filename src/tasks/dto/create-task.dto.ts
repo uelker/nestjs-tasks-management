039e2346e6f05ba58fd60783490dd02fbbecae93
Enes Ülker <enes.uelker@adesso.de>
2023-03-09 15:23:24 +0100

    feat(tasks): adding validation to dto's

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,4 +1,11 @@
+import { IsNotEmpty, IsString } from 'class-validator';
+
 export class CreateTaskDto {
-  title: string;
+  @IsString()
+  @IsNotEmpty()
   description: string;
+
+  @IsString()
+  @IsNotEmpty()
+  title: string;
 }
